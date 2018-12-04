e279d36929093b620912c0979b8ff22a7787b854
DESKTOP-DLGH8VC\maysw <mayswilliam@live.com>
2018-12-04 06:38:22 -0800

    William Mays save game

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -99,6 +99,23 @@ public class PlayerScript : MonoBehaviour
         }
     }
 
-    
+    public void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+
+    public void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+
+        playerHealth = data.health;
+
+        Vector3 position;
+        position.x = data.position[0];
+        position.y = data.position[1];
+        position.z = data.position[2];
+        transform.position = position;
+
+    }
 
 }
