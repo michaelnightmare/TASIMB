489853bce0f2fdd68ea4e5a79f095e6789636e6f
DESKTOP-DLGH8VC\maysw <mayswilliam@live.com>
2018-12-11 06:58:31 -0800

    Play from new main menu

---
+++ Assets/Cutscene/MainMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public Animator animator;
+
+    public void Start()
+    {
+        Invoke("FadeInToScene", 5);
+    }
+
+
+    void FadeInToScene()
+    {
+        animator.SetTrigger("FadeIn");
+    }
+
+    void ChangeLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StartGame()
+    {
+        animator.SetTrigger("FadeOut");
+        Invoke("ChangeLevel", 1);
+    }
+
+
+}
