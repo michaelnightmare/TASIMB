1939dc05bca95f693ce4ad3b2be9e79cc50717f9
michaelnightmare <michaelsmith0802@gmail.com>
2019-01-28 00:48:00 -0800

    Additional Weapons added as pickups- Still to implement the usage of them.

---
+++ Assets/Scripts/ShotgunScr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunScr : MonoBehaviour
+{
+    public float speed = 10f;
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            Debug.Log("Picked Up Shotgun");
+            Destroy(gameObject);
+
+        }
+    }
+}
